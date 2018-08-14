acd0f100fa4f3b032e43976968fa861ec2e75e32
pxgamer <owzie123@gmail.com>
2018-08-14 22:56:59 +0100

    Bind Base58 class as a singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use StephenHill\Base58;
 
 /**
  * Class AppServiceProvider
  */
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * All of the container singletons that should be registered.
+     *
+     * @var array
+     */
+    public $singletons = [
+        Base58::class => Base58::class,
+    ];
+
     /**
      * Bootstrap any application services.
      *
