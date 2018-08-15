169ac0bc6d568666f9bf8489f61f51a030288d92
pxgamer <owzie123@gmail.com>
2018-08-15 23:57:19 +0100

    Add model for the transactions table

---
+++ app/Transaction.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * Class Transaction
+ *
+ * @property string $id
+ * @property string $block
+ * @property int    $height
+ * @property string $dst
+ * @property float  $val
+ * @property float  $fee
+ * @property string $signature
+ * @property int    $version
+ * @property string $message
+ * @property Carbon $date
+ * @property string $public_key
+ */
+class Transaction extends Model
+{
+    /**
+     * @var bool
+     */
+    public $incrementing = false;
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'val' => 'float',
+        'fee' => 'float',
+    ];
+    /**
+     * @var array
+     */
+    protected $dates = [
+        'date',
+    ];
+}
