c8600222ffe4b54ad4c5dae30064983b5e786295
pxgamer <owzie123@gmail.com>
2018-08-15 22:42:02 +0100

    Move public and private key to PHPUnit config env

--- tests/Unit/Helpers/EllipticCurveTest.php
+++ tests/Unit/Helpers/EllipticCurveTest.php
@@ -10,18 +10,13 @@ use Tests\TestCase;
  */
 class EllipticCurveTest extends TestCase
 {
-    // phpcs:disable Generic.Files.LineLength
-    private const PRIVATE_KEY = 'Lzhp9LopCN52nowTXeeYixvrfHTJA1geEDuc7tg9Xyovg89rkwJ4sAbyhuu9h69n9fcFhN7814oA1QnDaS8wJ8ZPFrSL33ekezantdR9aW3WiHm3wM7ZtGxRTjs5BVzabzJ6AfHmmSBCackXkKBdsKo6swtqVqMUF';
-    private const PUBLIC_KEY = 'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSD1BMH8hd16tLkJWkpFAKV9UzeroAquAGP7rWzPk18SM5i7aPszwfBA7AaSMJy4i42hq7Bb7D8UZxe2WjTHmqeyVEB';
-    // phpcs:enable
-
     /**
      * @test
      * @return void
      */
     public function itCanSignAStringWithAPrivateKey()
     {
-        $result = EllipticCurve::sign('data', self::PRIVATE_KEY);
+        $result = EllipticCurve::sign('data', env('TEST_KEY_PRIVATE'));
 
         $this->assertInternalType('string', $result);
     }
@@ -35,7 +30,7 @@ class EllipticCurveTest extends TestCase
         $result = EllipticCurve::verify(
             'data',
             'iKx1CJNmCAJ82aQZjhPfdiiSsXGMZbGBqpBSXV66n7g335fJ9oevVTpxG4BokhnoUpvTD8rKXoVjbWM9rk7KyzW9SADest9Bsq',
-            self::PUBLIC_KEY
+            env('TEST_KEY_PUBLIC')
         );
 
         $this->assertTrue($result);
